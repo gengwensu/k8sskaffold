f3f0bf8450f395ac866ca2f2669595c328d81e41
GarySu <Gary.Su@hearst.com>
2021-12-16 13:40:59 -0500

    Initial commit

---
+++ src/main/java/com/gsucode/k8sskaffold/K8sskaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.gsucode.k8sskaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sskaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sskaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsucode/k8sskaffold/K8sskaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsucode.k8sskaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sskaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
